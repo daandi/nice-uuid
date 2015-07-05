c608c84d2c49af6bc0c434ee10bdaa2d5c61d94d
Andreas Neumann <andreas@neumann.biz>
2015-07-05 13:57:12 +0200

    Specs and working compile time check for valid UUID Strings. Next Goal add better Type Feedback.

--- src/test/scala/biz/neumann/NiceUUIDSpec.scala
+++ src/test/scala/biz/neumann/NiceUUIDSpec.scala
@@ -4,6 +4,8 @@ import java.util.UUID
 
 import org.scalatest.{Matchers, FlatSpec, WordSpec}
 
+import scala.util.Success
+
 /**
  * Andreas Neumann
  * Email: andreas@neumann.biz
@@ -19,7 +21,7 @@ class NiceUUIDSpec extends FlatSpec with Matchers {
   val exampleUUID : UUID =  UUID fromString  exampleUUIDString
 
   "Nice UUIDs" should "have an implicit conversion form String to UUID" in {
-    "16da0319-4762-4af1-93c1-5dcef6145d03".uuid should be(exampleUUID)
+    "16da0319-4762-4af1-93c1-5dcef6145d03".uuid should be( Success(exampleUUID) )
   }
 
   it should "provide a string Interpolator for UUIDs" in {
@@ -28,8 +30,22 @@ class NiceUUIDSpec extends FlatSpec with Matchers {
   
   it should "provide an implicit conversion from UUID to String" in {
     implicitly[String](exampleUUID) should be(exampleUUIDString)
-    
-    
   }
+
+  "Nice UUID Macros" should "allow strings in source code that encode an UUID" in {
+    // if this doesnt work the test would not compile
+    stringEncodingUUID("16da0319-4762-4af1-93c1-5dcef6145d03") should be(exampleUUIDString)
+  }
+
+  /*
+  If you uncomment this the tests will fail telling you that:
+  Error:(39, 23) You provided a malformed UUID: abc
+    stringEncodingUUID("abc")
+                      ^
+
+  it should "not compile" in {
+    stringEncodingUUID("abc")
+  }
+  */
 
 }
